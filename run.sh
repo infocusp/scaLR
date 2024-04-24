fae42edf80ba1408886628df42cbc9ff6987931e
Saiyam Jogani <saiyam@infocusp.com>
2024-04-24 13:53:17 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Feature chunking
-/home/saiyam/anaconda3/envs/bioc/bin/python /home/saiyam/projects/biocusp/scripts/extract_features.py --config /home/saiyam/projects/biocusp/scripts/config.yml --log
+# /home/saiyam/anaconda3/envs/bioc/bin/python /home/saiyam/projects/biocusp/scripts/extract_features.py --config /home/saiyam/projects/biocusp/scripts/config.yml --log
 
 # Training
 /home/saiyam/anaconda3/envs/bioc/bin/python /home/saiyam/projects/biocusp/scripts/train.py --config /home/saiyam/projects/biocusp/scripts/config.yml --log
