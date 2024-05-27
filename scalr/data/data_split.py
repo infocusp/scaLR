ec3b28abd2c94b5c0e94adf25eb7302e2c486257
SJo26 <saiyam@infocusp.com>
2024-05-27 14:07:20 +0530

    handling None stratification cases

--- scalr/data/data_split.py
+++ scalr/data/data_split.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 import anndata as ad
 from anndata import AnnData
-from sklearn.model_selection import GroupShuffleSplit
+from sklearn.model_selection import GroupShuffleSplit, StratifiedShuffleSplit
 
 from ..utils import write_data, dump_json, read_data
 
@@ -22,10 +22,10 @@ def _generate_train_val_test_split_indices(datapath: str,
         target: target for classification present in `obs`.
         stratify: optional parameter to stratify the split upon parameter.
         dirpath: dirpath to store generated split in json format
-    
+
     Returns:
         dict with 'train', 'test' and 'val' indices list.
-    
+
     """
 
     adata = read_data(datapath)
@@ -38,27 +38,31 @@ def _generate_train_val_test_split_indices(datapath: str,
     val_ratio = split_ratio[1] / total_ratio
     test_ratio = split_ratio[2] / total_ratio
 
-    test_splitter = GroupShuffleSplit(test_size=test_ratio,
+    Splitter = GroupShuffleSplit if stratify else StratifiedShuffleSplit
+
+    test_splitter = Splitter(test_size=test_ratio,
                                       n_splits=10,
                                       random_state=42)
     training_inds, testing_inds = next(
         test_splitter.split(
             metadata,
-            groups=metadata[stratify] if stratify is not None else None))
+            metadata[target],
+            groups=metadata[stratify] if stratify else None))
 
     if len(metadata[target].iloc[testing_inds].unique()) != n_cls:
         print('WARNING: All classes are not present in Test set')
 
     train_data = metadata.iloc[training_inds]
 
-    val_splitter = GroupShuffleSplit(test_size=val_ratio /
+    val_splitter = Splitter(test_size=val_ratio /
                                      (val_ratio + train_ratio),
                                      n_splits=10,
                                      random_state=42)
     fake_train_inds, fake_val_inds = next(
         val_splitter.split(
             train_data,
-            groups=train_data[stratify] if stratify is not None else None))
+            train_data[target],
+            groups=train_data[stratify] if stratify else None))
 
     true_test_inds = testing_inds.tolist()
     true_val_inds = train_data.iloc[fake_val_inds]['inds'].tolist()
