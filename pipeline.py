d3d963982c480a1cf42c35d174a2a95692049451
Amit Samal <amit.samal@minerva.infocusp.com>
2024-06-19 10:16:30 +0530

    added the set_seed function and ordered the libraries

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,25 @@
-import os
-import sys
 import argparse
+import os
+import random
+import sys
+
+import numpy as np
+import torch
+
 from config.utils import load_config
-from train import train
+from data_ingestion import ingest_data
 from evaluate import evaluate
 from feature_extraction import extract_features
-from data_ingestion import ingest_data
-import torch
-import random
-import numpy as np
+from train import train
+
+
+def set_seed(seed:int):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    
 
 def main():
     parser = argparse.ArgumentParser()
@@ -23,12 +34,7 @@ def main():
                         help='flag to store logs for the experiment')
 
     args = parser.parse_args()
-    seed = 42
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)     
+    set_seed(42)
 
     config = load_config(args.config)
     log = args.log
